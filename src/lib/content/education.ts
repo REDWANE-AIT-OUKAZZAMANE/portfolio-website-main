1a993089fa22c79abd872f4a38cf850d25bac468
REDWANE AIT OUKAZZAMANE <redkaz1328@gmail.com>
2025-07-02 19:45:15 +0100

    Update resume and education details: Revise resume content to reflect a transition to French, update work experience dates, and modify education entries to include a new preparatory class and adjusted degree periods.

--- src/lib/content/education.ts
+++ src/lib/content/education.ts
@@ -3,19 +3,20 @@ import { EducationSectionType } from '@/lib/types/sections';
 export const educationSection: EducationSectionType = {
   title: 'education',
   education: [
+    
     {
-      degree: 'Master of Science in Software Engineering',
-      institution: 'National Schools of Applied Sciences',
-      location: 'Tétouan',
-      period: 'Sep 2020 - Jun 2022',
-      gpa: '3.7 / 4.0',
-    },
-    {
-      degree: 'Bachelor\'s degree in computer science',
+      degree: 'Bachelor\'s degree in computer science SMI',
       institution: 'Abdelmalek Essaâdi University',
       location: 'Tétouan',
-      period: 'Sep 2018 - Jun 2020',
+      period: 'Sep 2019 - Jun 2024',
       gpa: '3.8 / 4.0',
     },
+    {
+      degree: 'Preparatory classes in mathematics and physics MP',
+      institution: 'Carnot prepa',
+      location: 'Meknes',
+      period: 'Sep 2016 - Jun 2018',
+      gpa: '17.5 / 20',
+    }
   ],
 }; 
